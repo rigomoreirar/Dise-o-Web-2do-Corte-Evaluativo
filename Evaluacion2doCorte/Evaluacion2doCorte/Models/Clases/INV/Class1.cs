ea960910722f22ad2d3e266ec645ae7dc0cfc7fe
Rigoberto Moreira <rigmoreirar@gmail.com>
2022-04-27 21:35:36 -0600

    Proyecto web

---
+++ Evaluacion2doCorte/Evaluacion2doCorte/Models/Clases/INV/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evaluacion2doCorte.Models.Clases.INV
+{
+    public class Unidad
+    {
+
+    }
+}
